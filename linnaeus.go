87eb5489ce4af1ef71b7faaf65959db0b8bac7ee
mbc <malcolmbalch@gmail.com>
2017-11-12 13:47:05 -0500

    Continue gluing things together

--- linnaeus.go
+++ linnaeus.go
@@ -1,16 +1,49 @@
 package main
 
+import "net/http"
+import "encoding/json"
+import "io/ioutil"
+import "fmt"
+
 type LinnaeusJob struct {
-	linnaeusPort int
+	ClassifierPort int
+	Conf           *Configuration
+	Pg             *PostgresClient
+}
+
+// Format of linnaeus response
+type Class struct {
+	ClassId       string  // "n00000"
+	HumanReadable string  // "corgi, pembrook welsh"
+	Probability   float64 // 0.76
 }
 
 // Goes through unclassified images and has Linnaeus classify them
 func (lj *LinnaeusJob) ClassifyImages() {
+	fmt.Println("Classifying images...")
+	unclassified := Pg.GetUnclassified()
 
+	for _, fname := range unclassified {
+		url := fmt.Sprintf("http://localhost:%s?filename=%s", lj.ClassifierPort, fname)
+		resp, err := http.Get(url)
+		if err != nil {
+			// TODO: probably *don't* want to panic here
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		classes := make([]Class, 0)
+		json.Unmarshal(body, &classes)
+
+		// TODO: Do stuff with classes
+		fmt.Println(classes)
+	}
 }
 
-func NewLinnaeusJob(port int) *LinnaeusJob {
+func NewLinnaeusJob(conf Configuration) *LinnaeusJob {
 	job := new(LinnaeusJob)
-	job.linnaeusPort = port
+	job.ClassifierPort = conf.ClassifierPort
+	job.pg = NewPostgresClient(job.Conf.PGHost, job.Conf.PGPort,
+		job.Conf.PGUser, job.Conf.PGPassword, job.Conf.PGDbname)
 	return job
 }
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "encoding/json"
+import "os"
+
+// Configuration struct that conf json file is read into
+type Configuration struct {
+	LinnaeusPort int
+	PGHost       string
+	PGPort       int
+	PGUser       string
+	PGPassword   string
+	PGDbname     string
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("Usage: ./main <absolute path to configuration file>")
+		return
+	}
+	file, _ := os.Open(args[0])
+	decoder := json.NewDecoder(file)
+	var conf = Configuration{}
+	err := decoder.Decode(&conf)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	job = NewLinnaeusJob(&conf)
+	job.ClassifyImages()
+}
